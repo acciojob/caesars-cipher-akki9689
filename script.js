24b959519e6b76e6841cb23970a7568774ac7978
Akash Puri <104100720+akki9689@users.noreply.github.com>
2023-10-18 02:16:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -32,10 +32,16 @@ const lookup = {
 };
 
 function rot13(encodedStr) {
-  let decodedArr = []; // Your Result goes here
-  // Only change code below this line
+  function decodeChar(char) {
+    const charCode = char.charCodeAt(0);
+    if (char >= 'A' && char <= 'Z') {
+      return String.fromCharCode(((charCode - 'A'.charCodeAt(0) + 13) % 26) + 'A'.charCodeAt(0));
+    }
+    return char;
+  }
 
-  return; //return decodedArr
+  // Use the decodeChar function to decode the entire string
+  return str.split('').map(decodeChar).join('');
 }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
